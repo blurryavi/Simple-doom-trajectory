73f2a5f93c1f9c35002211b30dd549dfe9e6959c
blurryavi <salitoavilatorres@gmail.com>
2025-11-07 10:49:52 -0500

    Terminando codigo sql

--- trajectory-doom.sql
+++ trajectory-doom.sql
@@ -1,75 +1,94 @@
-CREATE DATABASE trajectory_doom;
-CREATE TABLE telemetry (
-  telemetry_id int NOT NULL,
-  momentum numeric(5,2),
-  -- stats
-  x_axis numeric(5,2),
-  y_axis numeric(5,2),
-  z_axis numeric(5,2),
-  health int,
-  ammo int,
-  armor int,
-  player_id int,
-  tic_id timestamp,
-  FOREIGN KEY(player_id) REFERENCES player(player_id),
-  FOREIGN KEY(tic_id) REFERENCES tic(time),
-  PRIMARY KEY(telemetry_id)
+CREATE TABLE user_game (
+  user_id INT NOT NULL PRIMARY KEY,
+  age INT,
+  name VARCHAR(30),
+  gender VARCHAR(15)
+);
+
+CREATE TABLE player (
+  player_id INT NOT NULL PRIMARY KEY,
+  nickname VARCHAR(30),
+  session_id INT NOT NULL,
+  user_id INT,
+  FOREIGN KEY (user_id) REFERENCES user_game(user_id)
 );
 
 CREATE TABLE tic (
-  -- tic_id sera la resta entre el tiempo en el que se inicio la partida y el tiempo actual
-  tic_id int,
-  time timestamp,
-  PRIMARY KEY(tic_id)
+  tic_id INT PRIMARY KEY,
+  game_id int,
+  time TIMESTAMP NOT NULL,
+  FOREIGN KEY(game_id) REFERENCES game(game_id)
 );
 
-CREATE TABLE player(
-  player_id int NOT NULL,
-  nickname varchar(30),
-  session_id int NOT NULL,
-  user_id int,
-  PRIMARY KEY(player_id),
-  FOREIGN KEY(user_id) REFERENCES user(user_id)
+CREATE TABLE telemetry (
+  telemetry_id INT NOT NULL PRIMARY KEY,
+  momentum NUMERIC(5,2),
+  x_axis NUMERIC(5,2),
+  y_axis NUMERIC(5,2),
+  z_axis NUMERIC(5,2),
+  health INT,
+  ammo INT,
+  armor INT,
+  tic_id INT,
+  FOREIGN KEY (tic_id) REFERENCES tic(tic_id)
 );
 
-CREATE TABLE game(
-  game_id int NOT NULL,
-  player_id int NOT NULL,
-  started timestamp,
-  ended timestamp,
-  PRIMARY KEY(game_id),
-  FOREIGN KEY(player_id) REFERENCES player(player_id)
+CREATE TABLE game (
+  game_id INT NOT NULL PRIMARY KEY,
+  player_id INT NOT NULL,
+  started TIMESTAMP,
+  ended TIMESTAMP,
+  FOREIGN KEY (player_id) REFERENCES player(player_id)
 );
-CREATE TABLE user(
-  user_id int NOT NULL,
-  age int,
-  name varchar(30),
-  gender varchar(15),
-  PRIMARY KEY(user_id) ---- no sé si meter una foreign key para que se conecte con player
 
+CREATE TABLE UXInstrument (
+  instrument_id INT NOT NULL PRIMARY KEY,
+  num_item INT,
+  description VARCHAR(100),
+  type_instrument VARCHAR(6)
 );
-CREATE TABLE UXResponse(
-  response_id int NOT NULL,
-  instrument_id int NOT NULL,
-  user_id int NOT NULL,
-  item_id int NOT NULL,
-  answer         -- creo otra tabla para answer? porque un usuario tiene muchas respuestas
-);
-CREATE TABLE UXInstrument(
-  instrument_id int NOT NULL,
-  item_id int NOT NULL, -- Sería bueno añadir este atributo, ya que según el esquema un instrumento contiene un item, por lo tanto, se deben relacionar mediante una llave foránea
-  num_item int,
-  description varchar(100),
-  type_instrument varchar(6),
-  PRIMARY KEY(instrument_id),
-  FOREIGN KEY(item_id) REFERENCES item(item_id)
-);
+
 CREATE TABLE Item (
-  item_id int NOT NULL,
-  instrument_id int NOT NULL,
-  statement varchar(50),
-  answer int, -- pero acá habría varias
-  PRIMARY KEY(item_id),
+  item_id INT NOT NULL PRIMARY KEY,
+  instrument_id INT NOT NULL,
+  statement VARCHAR(50),
   FOREIGN KEY (instrument_id) REFERENCES UXInstrument(instrument_id)
 );
 
+CREATE TABLE UXResponse (
+  response_id INT NOT NULL PRIMARY KEY,
+  instrument_id INT NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (instrument_id) REFERENCES UXInstrument(instrument_id),
+  FOREIGN KEY (user_id) REFERENCES user_game(user_id)
+);
+
+CREATE TABLE Item_response (
+  itemresponse_id INT NOT NULL PRIMARY KEY,
+  response INT CHECK (response BETWEEN 1 AND 7),
+  response_id INT NOT NULL,
+  item_id INT NOT NULL,
+  FOREIGN KEY (response_id) REFERENCES UXResponse(response_id),
+  FOREIGN KEY (item_id) REFERENCES Item(item_id)
+);
+
+CREATE TABLE Map (
+  map_id int NOT NULL PRIMARY KEY,
+  name VARCHAR(30),
+  game_id int,
+  FOREIGN KEY(game_id) REFERENCES game(game_id)
+);
+
+CREATE TABLE Episode (
+  episode_id int NOT NULL PRIMARY KEY,
+  name VARCHAR(30),
+  map_id int,
+  FOREIGN KEY(map_id) REFERENCES Map(map_id)
+);
+
+CREATE TABLE Sector (
+  sector_id int NOT NULL PRIMARY KEY,
+  name VARCHAR(30),
+  episode_id int,
+  FOREIGN KEY(episode_id) REFERENCES Episode(episode_id)
+);
