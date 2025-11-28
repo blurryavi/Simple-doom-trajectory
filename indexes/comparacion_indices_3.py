ffe7d5d8534548649888642c0cd84c9c2cd0614d
blurryavi <salitoavilatorres@gmail.com>
2025-11-28 01:22:41 -0500

    Moviendo indices...

---
+++ indexes/comparacion_indices_3.py
@@ -0,0 +1,30 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Datos
+labels = ['Antes del índice', 'Después del índice']
+execution_times = [5.028, 3.484]  # ms
+
+x = np.arange(len(labels))
+
+plt.figure()
+bars = plt.bar(x, execution_times, width=0.4)
+
+# Etiquetas encima de cada barra
+for bar in bars:
+    height = bar.get_height()
+    plt.text(
+        bar.get_x() + bar.get_width() / 2,
+        height,
+        f'{height:.3f} ms',
+        ha='center',
+        va='bottom'
+    )
+
+plt.xticks(x, labels)
+plt.ylabel('Execution Time (ms)')
+plt.title('Comparación del tiempo de ejecución del query')
+
+plt.tight_layout()
+plt.show()
